89297af9a0c804c5d0ea2f1642531fd634342de7
PJ Yong <pj.cemat@gmail.com>
2022-04-12 23:14:22 -0700

    minor changes

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ for (let i = 0; i < 256; i++) {
     let myDivs = document.createElement("div");
     myDivs.style.border = 'thin solid blue'
     container.appendChild(myDivs);
-    myDivs.innerText = i;
 
+    // adds event to change background of divs when moused over 
     myDivs.addEventListener("mouseover", function () {
         myDivs.style.backgroundColor = 'green';
     });
