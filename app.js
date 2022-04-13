44bbc75c7355e63b482d3556113406422a1f6f60
PJ Yong <pj.cemat@gmail.com>
2022-04-12 22:48:50 -0700

    verified mouseover is working

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const container = document.querySelector('#container')
 //Generates 266 Divs elements
 for (let i = 0; i < 256; i++) {
     let myDivs = document.createElement("div");
-    myDivs.style.border = 'thin solid red'
+    myDivs.style.border = 'thin solid blue'
     container.appendChild(myDivs);
-    myDivs.innerText = i
+    myDivs.innerText = i;
+
+    myDivs.addEventListener("mouseover", function () {
+        myDivs.style.backgroundColor = 'green';
+    });
 }
+
+
