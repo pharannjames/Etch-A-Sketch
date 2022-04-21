d7cc632cd2315d0b2ba4fd4268b3b03ee927238b
PJ Yong <pj.cemat@gmail.com>
2022-04-20 22:44:37 -0700

    added main code to functions for testing

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
-const container = document.querySelector('#container')
 const reset = document.querySelector('#reset')
+generateGrid(4);
 
-//Generates 266 Divs elements
-for (let i = 0; i < 256; i++) {
-    let myDivs = document.createElement("div");
-    myDivs.style.border = 'thin solid blue'
-    container.appendChild(myDivs);
+function generateGrid(size) {
+    const container = document.querySelector('#container')
+    container.style.gridTemplateColumns = `repeat(${size} , 1fr)`
+    container.style.gridTemplateRows = `repeat(${size} , 1fr)`
 
-    // adds event to change background of divs when moused over
-    myDivs.addEventListener("mouseover", function () {
-        myDivs.style.backgroundColor = 'green';
-    });
+    let gridSize = size * size;
+    for (let i = 0; i < gridSize; i++) {
+        let myDivs = document.createElement("div");
+        myDivs.innerHTML = i;
+        myDivs.style.border = 'thin solid blue'
+        container.appendChild(myDivs);
+        // adds event to change background of divs when moused over
+        myDivs.addEventListener("mouseover", function () {
+            myDivs.style.backgroundColor = 'green';
+        });
 
+        // resets grid 
+        reset.addEventListener('click', function () {
+            myDivs.style.backgroundColor = 'white';
+            // let gridSize = prompt('Please enter new grid size');
+        })
 
-    // resets grid 
-    reset.addEventListener('click', function () {
-        myDivs.style.backgroundColor = 'white';
-        let gridSize = prompt('Please enter new grid size');
-    })
+    }
+
 }
+
+function changeGridSize(input) {
+    generateGrid(input);
+}
+
